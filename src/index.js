3d470aed7e2db3654a510da57cdcc0a61abd4fb7
wajdi kanzali <kanzali.wajdi@gmail.com>
2018-01-11 15:20:24 +0100

    React router

--- src/index.js
+++ src/index.js
@@ -3,13 +3,29 @@ import React from 'react';
 import { render } from 'react-dom';
 
 //Components
-//import Connexion from './components/Connexion';
+import Connexion from './components/Connexion';
 import App from './components/App';
+import NotFound from './components/NotFound';
+
+//Rooter
+import { BrowserRouter, Match, Miss } from 'react-router';
 
 //CSS
 import  './index.css';
 
+const Root = () => {
+	return (
+		<BrowserRouter>
+			<div>
+				<Match exactly pattern="/" component={Connexion}/>
+				<Match exactly pattern="/pseudo/:pseudo" component={App}/>
+				<Miss component={NotFound}/>
+			</div>
+		</BrowserRouter>
+	)
+}
+
 render(
-	<App/>,
+	<Root/>,
 	document.getElementById('root')
 );
