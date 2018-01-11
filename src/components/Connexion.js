165386a897eaa784985250adc36a9f3f95955f94
wajdi kanzali <kanzali.wajdi@gmail.com>
2018-01-11 15:49:40 +0100

    use Event

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -2,11 +2,23 @@
 import React from 'react';
 
 class Connexion extends React.Component {
+
+	goToChat = event => {
+		event.preventDefault();
+		// Récupérer le pseudo
+		console.log(this.pseudoInput.value);
+	};
+
+
 	render(){
 		return (
-			<div className="connexionBox">
+			<div className="connexionBox" onSubmit={e => this.goToChat(e)}>
 			 <form className="connexion">
-				<input type="text" placeholder="Pseudo" required/>
+				<input 
+				 type="text" 
+				 placeholder="Pseudo" 
+				 required
+				 ref={input => {this.pseudoInput = input }} />
 				<button type="submit">GO</button>
 			 </form>
 			</div>
