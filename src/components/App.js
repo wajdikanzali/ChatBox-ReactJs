f6d1bb6ac3adbbdf12317af8548a3a5e4e3edd06
wajdi kanzali <kanzali.wajdi@gmail.com>
2018-01-15 11:36:42 +0100

    Add CSS animation

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,10 @@ import Formulaire from './Formulaire';
 import Message from './Message';
 import base from '../base';
 
+//CSS
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import '../animation.css';
+
 class App extends React.Component {
 
 
@@ -20,6 +24,11 @@ class App extends React.Component {
 		});
 	}
 
+	componentDidUpdate() {
+		// Mettre le scroll en bas
+		this.messages.scrollTop = this.messages.scrollHeight;
+	}
+
 	addMessage = message => {
 		// Copier le state
 		const messages = {...this.state.messages};
@@ -32,18 +41,30 @@ class App extends React.Component {
 		this.setState({ messages });
 	};
 
+	isUser = (pseudo) => {
+		return pseudo === this.props.params.pseudo;
+	};
+
 	render(){
 
 		const messages = Object
 		  .keys(this.state.messages)
-		  .map(key => <Message key={key} details={this.state.messages[key]} />)
+		  .map(key => <Message key={key} details={this.state.messages[key]} isUser={this.isUser} />)
 		;
 
 		return (
 			<div className="box">
 			 <div>
-			 	<div className="messages">
-			 	 {messages}
+			 	<div className="messages" ref={input => this.messages = input}>
+			 	<ReactCSSTransitionGroup 
+			 		component="div"
+			 		className="message"
+			 		transitionName="message"
+			 		transitionEnterTimeout={200}
+			 		transitionLeaveTimeout={200}
+			 	>
+			 		{messages}
+			 	</ReactCSSTransitionGroup>
 			 	</div>
 			 	<Formulaire addMessage={this.addMessage} pseudo={this.props.params.pseudo} length="140"/>
 			 </div>
